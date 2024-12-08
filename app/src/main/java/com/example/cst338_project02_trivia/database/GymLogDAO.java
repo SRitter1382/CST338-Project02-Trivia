1df84a3e5850fa2bf2f05d676fcc7e38ba998a62
SRitter1382 <sritter@csumb.edu>
2024-12-08 13:30:32 -0800

    Temp 3

--- app/src/main/java/com/example/cst338_project02_trivia/database/GymLogDAO.java
+++ app/src/main/java/com/example/cst338_project02_trivia/database/GymLogDAO.java
@@ -12,7 +12,11 @@ import com.example.cst338_project02_trivia.database.entities.GymLog;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Dao
 public interface GymLogDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(GymLog gymlog);
 
+    @Query("Select * from " + GymLogDatabase.GYM_LOG_TABLE)
+    ArrayList<GymLog> getAllRecords();
 }
--- app/src/main/java/com/example/cst338_project02_trivia/database/GymLogRepository.java
+++ app/src/main/java/com/example/cst338_project02_trivia/database/GymLogRepository.java
@@ -12,5 +12,35 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class GymLogRepository {
+    private GymLogDAO gymLogDAO;
+    private ArrayList<GymLog> allLogs;
+    public GymLogRepository(Application application){
+        GymLogDatabase db = GymLogDatabase.getDatabase(application);
+        this.gymLogDAO = db.gymLogDAO();
+        this.allLogs = this.gymLogDAO.getAllRecords();
+    }
 
+    public ArrayList<GymLog> getAllLogs(){
+        Future<ArrayList<GymLog>> future = GymLogDatabase.databaseWriteExecutor.submit(new Callable<ArrayList<GymLog>>() {
+            @Override
+            public ArrayList<GymLog> call() throws Exception {
+                return gymLogDAO.getAllRecords();
+            }
+        });
+        try {
+            return future.get();
+        } catch(InterruptedException | ExecutionException e){
+            e.printStackTrace();
+            Log.i(MainActivity.TAG, "Problem in executor when getting all GymLogs in Repository");
+        }
+        return null;
+    }
+
+    public void insertGymLog(GymLog gymLog){
+        GymLogDatabase.databaseWriteExecutor.execute(() ->
+                {
+                    gymLogDAO.insert(gymLog);
+                }
+                );
+    }
 }
